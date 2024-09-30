3cad0ee0edee5d3fa03187df3180bb7a855e445c
shohaib1996 <khanshohaibhossain@gmail.com>
2024-09-30 09:41:02 +0600

    Book now button re directs done

--- src/pages/home/banner/Banner.tsx
+++ src/pages/home/banner/Banner.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import meeting from "../../../assets/meeting.mp4";
 import Container from "../../../utils/container/Container";
 
@@ -17,9 +18,15 @@ const Banner = () => {
           </video>
           <div className="absolute top-0 bg-black bg-opacity-25 w-full h-full rounded-lg"></div>
           <div className="absolute top-1/3 left-[15%] text-center space-y-4">
-            <h1 className="lg:text-5xl text-xl text-white font-bold ">Book Your Ideal Meeting Room with Ease.</h1>
-            <p className="text-md text-slate-200 font-semibold">Efficient, hassle-free room booking for all your meeting needs.</p>
-            <button className="btn">Book Now</button>
+            <h1 className="lg:text-5xl text-xl text-white font-bold ">
+              Book Your Ideal Meeting Room with Ease.
+            </h1>
+            <p className="text-md text-slate-200 font-semibold">
+              Efficient, hassle-free room booking for all your meeting needs.
+            </p>
+            <Link to="/meeting-rooms">
+              <button className="btn">Book Now</button>
+            </Link>
           </div>
         </div>
       </Container>
